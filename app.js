55e1663a22ba0998f21ae5fe943a71b270dd780e
Aleksandr Zenkin <hypermyper@yandex.ru>
2021-02-12 20:07:55 +0300

    review1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
+
 const app = express();
 
 const { PORT = 3000 } = process.env;
@@ -13,7 +14,7 @@ app.use('/', usersRouter);
 app.use('/', cardsRouter);
 
 app.use('*', (req, res) => {
-  res.status(404).send({ "message": "Запрашиваемый ресурс не найден" });
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 app.listen(PORT, () => {
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,16 +1,15 @@
 const path = require('path');
 const getDataFromFile = require('../helpers/files');
+
 const dataPath = path.join(__dirname, '..', 'data', 'cards.json');
 
-const getCards = (req, res) => {
-    return getDataFromFile(dataPath)
-        .then(cards => {
-          if (!cards) {
-            return res.status(404).send({ message: 'Нет файла с данными'});
-          }
-          res.status(200).send(cards);
-        })
-        .catch(err => res.status(400).send(err));
-};
+const getCards = (req, res) => getDataFromFile(dataPath)
+  .then((cards) => {
+    if (!cards) {
+      return res.status(404).send({ message: 'Нет файла с данными' });
+    }
+    return res.status(200).send(cards);
+  })
+  .catch(() => res.status(500).send({ message: 'Запрашиваемый ресурс не найден' }));
 
 module.exports = { getCards };
